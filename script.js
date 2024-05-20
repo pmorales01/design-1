07a826bcd6683cf72abf2c69d594dca3d9c631d6
Pedro Morales <pedrojmorales01@gmail.com>
2024-05-19 21:29:46 -0700

    Added dropdown responsive navigation menu for screens <= 755 px wide

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var nav = document.getElementById('menu');
+    var toggle = document.getElementById('toggle');
+  
+    toggle.addEventListener('click', function() {
+      nav.classList.toggle('active');
+      console.log('CLICK');
+    });
+  });
+  
